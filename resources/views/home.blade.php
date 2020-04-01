c6e3f189fa3efd908c377ab94cc61969195ec28f
pledesma <pledesma@deloitte.com>
2020-04-01 12:10:06 -0500

    se instaló el nuevo tema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
 @section('content')
-<div class="container">
-    <h2>Products</h2>
+<!-- Start Category Area -->
+<section class="htc__category__area ptb--100">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="section__title--2 text-center">
 
-    <div class="row">
-        @foreach ($products as $product)
-            <div class="col-4">
-                <div class="card">
-                    <img class="card-img-top" src="{{ asset('/img/bulmaswatch-banner.png') }}" alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $product->name }}</h4>
-                        <p class="card-text">{{ $product->description }}</p>
-                        <h3>${{ $product->price }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <a href="{{ route('cart.add', $product->id) }}" class="card-link">Add to cart</a>
-                    </div>
+                    <h2 class="title__line">New Arrivals</h2>
+                    <p>Awesome montessori products</p>
                 </div>
             </div>
-        @endforeach
+        </div>
+        <div class="htc__product__container">
+
+            <div class="row">
+                <div class="product__list clearfix mt--30">
+                    <!-- Start Single Category -->
+                    @foreach ($products as $product)
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/1.jpg" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">{{ $product->name }}</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">$30.3</li>
+                                        <li>$ {{ $product->price }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</section>
+<!-- End Category Area -->
 @endsection
